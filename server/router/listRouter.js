b0770918c14e3256fcf04d4b2aa19f7ef323e7ea
Natalie McMann <nmcmann110@gmail.com>
2023-01-12 15:02:59 -0600

    added a router file for routes

---
+++ server/router/listRouter.js
@@ -0,0 +1,8 @@
+const { Router, application } = require('express');
+const express = require('express');
+const pool = require('../modules/pool');
+const listRouter = express.Router();
+
+// put routes here
+
+module.exports = listRouter;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+const listRouter = require('./router/listRouter.js');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
@@ -10,6 +11,7 @@ app.use(express.static('build'));
 /** ---------- EXPRESS ROUTES ---------- **/
 // Create your API routes in a separate file
 // and plug them in here with `app.use()`
+app.use('/list', listRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
