43e25ff53be43256745842431f8733e4becb24b5
Natalie McMann <nmcmann110@gmail.com>
2023-01-12 15:15:52 -0600

    get route server side completed

--- server/router/listRouter.js
+++ server/router/listRouter.js
@@ -4,5 +4,20 @@ const pool = require('../modules/pool');
 const listRouter = express.Router();
 
 // put routes here
+// GET //
+listRouter.get('/', (req, res) => {
+    console.log('in GET route');
+    let sqlQuery = `
+    SELECT * FROM "list"
+    ORDER BY "item" ASC
+    `;
+    pool.query(sqlQuery)
+}).then((dbRes) => {
+    res.send(dbRes.rows);
+}).catch((dbErr)=> {
+    console.log('GET route not working', dbErr);
+    res.sendStatus(500);
+})
+
 
 module.exports = listRouter;
