92aeb2df2214114681f6b9b91e1c5759c0c7b511
henrisoho <bakingwithmymother@gmail.com>
2023-01-12 15:17:36 -0600

    notes added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,25 @@ import './App.css';
 
 
 function App() {
+
+    const postList = () => {
+    axios.post('/list', { item : newItem, quantity: newQuantity, unit : newUnit, purchased : 0 })//<list item coresponding names with sql database// //^axios post with all items to be sent to server side for data base package//
+      .then(response => {
+        // ^response ready for use in rendering new list item//
+        setNewItem('');
+        setQuantity(0)
+        setNewUnit('')
+        setPurchasedStatus(false);
+        //^reset all form inputs to null//
+        getList();
+        //^rerender listItems on the dom//
+      })
+      .catch(err => {
+        alert('Error Adding Item');
+        console.log(err);
+      })
+  };
+
     return (
         <div className="App">
             <Header />
@@ -18,21 +37,7 @@ function App() {
 export default App;
 
 
-const postList = () => {
-    axios.post('/list', { item : newItem, quantity: newQuantity, unit : newUnit, purchased, purchasedStatus  })
-      .then(response => {
-        // clear inputs
-        setNewItem('');
-        setQuantity(0)
-        setNewUnit('')
-        setPurchasedStatus(false);
-        getList();
-      })
-      .catch(err => {
-        alert('Error Adding Item');
-        console.log(err);
-      })
-  };
+
 
 
 
