b13834247aa565bf5b1d5c834608586bfc6f7122
Lauren <lauren.a.heavey@gmail.com>
2023-01-13 11:13:56 -0600

    Add conditional styling to purchased list items.

--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -5,27 +5,51 @@ import Grid from '@mui/material/Unstable_Grid2';
 import PutButton from "./PutButton";
 import DeleteButton from "./DeleteButton";
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 function ListItems({itemList, getList}){
+
+const purchasedTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 return(
 <div>
-                    <Grid container spacing={2}>
-                    {itemList.map((item) => {
-                        return (
-                            <Grid xs={3} key={item.id}>
-                                <Card>
-                                    <CardContent>
-                                        <p>{item.item}</p> 
-                                        <p>{item.quantity} {item.unit}</p>
-                                        <p>
-                                          <PutButton item={item} getList={getList}/>
-                                          <DeleteButton item={item} getList={getList}/>
-                                        </p>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                                )})}
-                    </Grid>
+    <Grid container spacing={2}>
+    {itemList.map((item) => {
+        return (
+        <Grid xs={3} key={item.id}>
+        {item.purchased
+        ?
+        <ThemeProvider theme={purchasedTheme}>
+            <Card>
+                <CardContent>
+                    <p>{item.item}</p> 
+                    <p>{item.quantity} {item.unit}</p>
+                    <p>
+                    <PutButton item={item} getList={getList}/>
+                    <DeleteButton item={item} getList={getList}/>
+                    </p>
+                </CardContent>
+            </Card>
+        </ThemeProvider>
+        :
+        <Card>
+            <CardContent>
+                <p>{item.item}</p> 
+                <p>{item.quantity} {item.unit}</p>
+                <p>
+                <PutButton item={item} getList={getList}/>
+                <DeleteButton item={item} getList={getList}/>
+                </p>
+            </CardContent>
+        </Card>
+        }
+        </Grid>
+        )})}
+    </Grid>
 
  </div>
 )}
